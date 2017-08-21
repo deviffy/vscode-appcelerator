f7a80b3b6d84c75956e478995def297df4236a1d
Marco Palermo (MarcoDuff) <marco@marcoduff.com>
2017-08-21 15:59:02 +0200

    New location for settings

--- src/lib/AppceleratorBuild.ts
+++ src/lib/AppceleratorBuild.ts
@@ -5,7 +5,7 @@ import {IOsBuild} from './IOsBuild';
 
 var shell = require('shelljs');
 var project_flag = ' --project-dir "' + vscode.workspace.rootPath +'"';
-var config_file = vscode.workspace.rootPath+'/dist/vscode-appcelerator.config.json';
+var config_file = vscode.workspace.rootPath+'/.vscode-appcelerator/settings.json';
 var lastCmd;
 
 export abstract class AppceleratorBuild {
