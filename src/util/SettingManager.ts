b1741243d8a8ceac436949980086ac2afd986b3d
Marco Palermo (MarcoDuff) <marco@marcoduff.com>
2017-11-13 16:43:25 +0100

    Bugfix if setting file doesn't exist.

--- src/util/SettingManager.ts
+++ src/util/SettingManager.ts
@@ -5,8 +5,11 @@ var config_file = vscode.workspace.rootPath+'/.vscode-appcelerator/settings.json
 
 export class SettingManager {
     public static getConfig() {
-        var configText = fs.readFileSync(config_file, {"encoding":"utf8"});
-        return JSON.parse(configText);
+        if(fs.existsSync(config_file)) {
+            var configText = fs.readFileSync(config_file, {"encoding":"utf8"});
+            return JSON.parse(configText);
+        }
+        else return {};
     }
 
     public static updateConfig(newConfig) {
@@ -14,6 +17,7 @@ export class SettingManager {
         for(var element in newConfig) {
             config[element] = newConfig[element];
         }
+        if(!fs.existsSync(config_file)) fs.mkdirSync(vscode.workspace.rootPath+'/.vscode-appcelerator');
         fs.writeFile(config_file,JSON.stringify(config), function(err) {
             console.log("fs.writeFile", err);
         });
